f73b844b057bc1b0e789bba5295e83bc5574a386
kmlino <kmlino@outlook.com>
2023-09-21 20:39:26 -0300

    Improving validations and changing colors

--- react_version/src/App.js
+++ react_version/src/App.js
@@ -8,10 +8,14 @@ export default function App() {
   const operators = ['+', '-', '*', '/']
 
   const insertNum = (val) => {
+    if (input.length > 14) return;
+    // Incomplete logic *to repair*
+    if (input.split("").includes(".") && val === ".") return;
     setInput(input + val);
   }
 
   const insertOperator = (val) => {
+    if (input.length > 14) return;
     if (input === "" || (operators.includes(input[input.length - 1]) && operators.includes(val))) {
       return;
     } else {
@@ -24,7 +28,6 @@ export default function App() {
     if (input === "" || operators.includes(lIndex)) {
       return input;
     } else {
-      // console.log(evaluate(input))
       setInput(evaluate(input));
     }
     
@@ -32,34 +35,37 @@ export default function App() {
 
   return (
     <>
-    <div className="main-content">
-      <Button isInput>{input}</Button>
-      <div className="buttons">
-        <Button onClick={insertNum}>7</Button>
-        <Button onClick={insertNum}>8</Button>
-        <Button onClick={insertNum}>9</Button>
-        <Button onClick={insertOperator}>*</Button>
-      </div>
-      <div className="buttons">
-        <Button onClick={insertNum}>4</Button>
-        <Button onClick={insertNum}>5</Button>
-        <Button onClick={insertNum}>6</Button>
-        <Button onClick={insertOperator}>/</Button>
-      </div>
-      <div className="buttons">
-        <Button onClick={insertNum}>1</Button>
-        <Button onClick={insertNum}>2</Button>
-        <Button onClick={insertNum}>3</Button>
-        <Button onClick={insertOperator}>+</Button>
-      </div>
-      <div className="buttons">
-        <Button onClick={insertNum}>0</Button>
-        <Button onClick={insertNum}>.</Button>
-        <Button onClick={() => setInput("")}>C</Button>
-        <Button onClick={insertNum}>-</Button>
-      </div>
-      <div className="buttons">
-        <Button onClick={() => calc()}>=</Button>
+    <div className="app">
+    <h1>Calculadora React</h1>
+      <div className="main-content">
+        <Button isInput>{input}</Button>
+        <div className="buttons">
+          <Button onClick={insertNum}>7</Button>
+          <Button onClick={insertNum}>8</Button>
+          <Button onClick={insertNum}>9</Button>
+          <Button onClick={insertOperator}>*</Button>
+        </div>
+        <div className="buttons">
+          <Button onClick={insertNum}>4</Button>
+          <Button onClick={insertNum}>5</Button>
+          <Button onClick={insertNum}>6</Button>
+          <Button onClick={insertOperator}>/</Button>
+        </div>
+        <div className="buttons">
+          <Button onClick={insertNum}>1</Button>
+          <Button onClick={insertNum}>2</Button>
+          <Button onClick={insertNum}>3</Button>
+          <Button onClick={insertOperator}>+</Button>
+        </div>
+        <div className="buttons">
+          <Button onClick={insertNum}>0</Button>
+          <Button onClick={insertNum}>.</Button>
+          <Button onClick={() => setInput("")}>C</Button>
+          <Button onClick={insertNum}>-</Button>
+        </div>
+        <div className="buttons">
+          <Button onClick={() => calc()}>=</Button>
+        </div>
       </div>
     </div>
     </>
